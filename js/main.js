ccd4b055ea0427a2bd4a856c501243e378a60566
akbarinfo <girtbezbet@gmail.com>
2022-02-14 00:59:35 +0500

    update

---
+++ js/main.js
@@ -0,0 +1,49 @@
+// let open = document.getElementById('id-menu');
+// let navbox = document.getElementById('id-nav');
+// let icon = document.getElementById('id-icon');
+
+// let count = 0;
+// open.addEventListener('click', function () {
+//   navbox.classList.toggle('open');
+//   if (count == 0) {
+//     icon.style.fontSize = '32px';
+//     icon.className = "bx bx-x";
+//     count++;
+//   } else {
+//     icon.className = "bx bx-menu";
+//     count = 0;
+//   }
+// })
+
+
+let obtn = document.getElementById('id-open-menu');
+let xbtn = document.getElementById('id-remove-menu');
+let menubox = document.getElementById('id-menubox');
+
+obtn.addEventListener(
+  'click', function(){
+    menubox.classList.add('opens');
+  }
+);
+
+xbtn.addEventListener('click', function() {
+  menubox.classList.remove('opens');
+});
+
+
+let cursor = document.querySelector(".cursor");
+let cursorSpot = document.querySelector(".cursor-spot");
+
+document.addEventListener("mousemove", function(b){
+  cursor.style.cssText = cursorSpot.style.cssText = "left: " + b.clientX + "px; top: " + b.clientY + "px;";
+
+  const anchors =  document.querySelectorAll("a");
+  for(let a of anchors) {
+    a.onmouseover = () => {
+      cursor.classList.add("chover")
+    }
+    a.onmouseout = () => {
+      cursor.classList.remove("chover")
+    }
+  }
+});
