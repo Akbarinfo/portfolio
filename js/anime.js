87dc55007d25fef0c78a9578a81f80f3c3395e1b
akbarinfo <girtbezbet@gmail.com>
2022-02-14 23:38:15 +0500

    update

---
+++ js/anime.js
@@ -0,0 +1,66 @@
+ScrollReveal({
+  reset: true,
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+});
+
+ScrollReveal().reveal('.header__banner', {
+  delay: 200
+});
+ScrollReveal().reveal('.about__title', {
+  delay: 300
+});
+ScrollReveal().reveal('.about__box', {
+  delay: 300,
+  origin: 'left'
+});
+ScrollReveal().reveal('.about__user', {
+  delay: 300,
+  origin: 'right'
+});
+ScrollReveal().reveal('.skils__title', {
+  delay: 300,
+  origin: 'top'
+});
+ScrollReveal().reveal('.skils__bar', {
+  delay: 300,
+  origin: 'left'
+});
+ScrollReveal().reveal('.skils__bar-second', {
+  delay: 300,
+  origin: 'right'
+});
+ScrollReveal().reveal('.skils__bar-third', {
+  delay: 300,
+  origin: 'left'
+});
+ScrollReveal().reveal('.portfolio__list', {
+  delay: 300,
+  origin: 'left'
+});
+ScrollReveal().reveal('.portfolio__item', {
+  delay: 300,
+  origin: 'top'
+});
+ScrollReveal().reveal('.contact__content', {
+  delay: 300,
+  origin: 'bottom'
+});
+ScrollReveal().reveal('.contact__form', {
+  delay: 300,
+  origin: 'top'
+});
+ScrollReveal().reveal('.footer__desc', {
+  delay: 300,
+  origin: 'bottom'
+});
+ScrollReveal().reveal('.footer__item', {
+  delay: 300,
+  origin: 'bottom',
+  interval: 200
+});
+ScrollReveal().reveal('.footer__text', {
+  delay: 300,
+  origin: 'bottom'
+});
