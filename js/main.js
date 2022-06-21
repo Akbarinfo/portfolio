6aca42c350cea24e7d793e3443d7a2aea44ef1b7
Akbarinfo <girtbezbet@gmail.com>
2022-06-21 14:54:48 +0500

    updated

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-// let open = document.getElementById('id-menu');
-// let navbox = document.getElementById('id-nav');
-// let icon = document.getElementById('id-icon');
-
-// let count = 0;
-// open.addEventListener('click', function () {
-//   navbox.classList.toggle('open');
-//   if (count == 0) {
-//     icon.style.fontSize = '32px';
-//     icon.className = "bx bx-x";
-//     count++;
-//   } else {
-//     icon.className = "bx bx-menu";
-//     count = 0;
-//   }
-// })
-
-
 let obtn = document.getElementById('id-open-menu');
 let xbtn = document.getElementById('id-remove-menu');
 let menubox = document.getElementById('id-menubox');
